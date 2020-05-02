8471ed6034ea570c434b5fd8419959bbabcc0788
Christopher Kelly <chris@nourishbalancethrive.com>
2020-05-02 13:46:17 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nbthaq',
+      version='0.1',
+      description='Nourish Balance Thrive Health Assessment Questionnaire',
+      url='http://github.com/cck197/nbthaq',
+      author='Christopher Kelly',
+      author_email='chris@nourishbalancethrive.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
